3e1d056c7c0232661c95d625cf0945b48cf479e2
Chrisi <>
2022-03-28 22:42:31 +0200

    Added test program

--- source/ConsoleApp1/Program.cs
+++ source/ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Drawing;
+﻿using System;
 using GraphMLWriter.Elements;
 using GraphMLWriter.Elements.Edges;
 using GraphMLWriter.Elements.Shapes;
@@ -12,54 +12,33 @@ namespace ConsoleApp1
         {
             var n1 = new Node("Node A", 0)
             {
-                X = 0,
-                Y = 0,
-                Width = 80
+                Y = 80,
+                Width = 100
             };
-
             var n2 = new Node("Node B", 1)
             {
-                X = 200,
-                Y = 0,
-                Width = 80,
-                Color = "#ffcc00"
+                Width = 100
             };
-
             var n3 = new Node("Node C", 2)
             {
-                X = 0,
-                Y = 80,
-                Width = 80
+                Y = 160,
+                Width = 100
             };
 
             var graph = new Graph();
-
             graph.AddNodes(n1, n2, n3);
 
             graph.AddEdge(new Edge(0, n1, n2)
             {
                 TargetArrow = EdgeArrow.WhiteDelta,
-                LineProperties =
-                {
-                    Color = "#777777",
-                    Width = 3d
-                }
             });
-
             graph.AddEdge(new Edge(1, n1, n3)
             {
-                TargetArrow = EdgeArrow.WhiteDiamond,
-                Points =
-                {
-                    new Point(-40, 15),
-                    new Point(-40, 95)
-                }
+                TargetArrow = EdgeArrow.Diamond,
             });
 
             var serializer = new GraphSerializer();
-            serializer.AddGraph(graph);
-            serializer.GetDocument().Save(@"E:\test.graphml");
-
+            serializer.Serialize(graph, @"E:\test.graphml");
         }
     }
 }
